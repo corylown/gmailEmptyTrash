972c03159abba3e49cc11cad0068e9f111b654a5
corylown@gmail.com <corylown@gmail.com>
2012-10-15 16:49:46 -0400

    added expunge class to Gmail class

--- gmailEmptyTrash.rb
+++ gmailEmptyTrash.rb
@@ -1,12 +1,24 @@
+# Require ruby-gmail gem
 require 'gmail'
 
 username = ''
 password = ''
 
+# Add expunge method to Gmail class of ruby-gmail gem
+class Gmail
+    def expunge
+        @imap.expunge
+    end
+end
+
+# Intantiate new Gmail instance with credentials
 Gmail.new(username, password) do |gmail|
-    
+
+    # Mark each email in Trash as deleted
     gmail.mailbox('[Gmail]/Trash').emails.each do |email|
         email.delete!
     end
+    
+    # Expunge emails marked as deleted (This is permanent!)
     gmail.expunge
 end
